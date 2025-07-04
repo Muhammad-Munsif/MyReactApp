927c027d0997e26a77423e7d103c64ae244c7f19
MUHAMMAD-MUNSIF <m.munsif4375@gmail.com>
2025-07-04 16:39:54 +0500

    lorem added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <div className="bg-rose-500 text-2xl c">
       <h1>Hello world this is my first react app i am very glad to start react jss</h1>
       <h1>Hi this is another heading i added</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, aspernatur minus neque dicta tempora harum necessitatibus quae provident. Cumque quisquam delectus veniam similique eius! Corrupti aperiam saepe corporis eaque esse, doloremque tempore quia delectus expedita rem eius qui praesentium reprehenderit!</p>
     </div>
   );
 }
